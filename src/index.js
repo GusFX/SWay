9d036adb92db12f8868ada7a81549cdf6baa14c2
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-16 21:56:37 -0300

    encapsulando app com paper provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './navigation';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 export default function Index() {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
