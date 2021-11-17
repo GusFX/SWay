a739ef3951c4bbeb87e5c4c871c388feae6bbf9d
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-16 21:26:30 -0300

    adicionando visualização do mapa

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+
+import MapView from 'react-native-maps';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Sou a tela principal, olha que top</Text>
+    <View style={{width: '100%', height: '100%'}}>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{flex: 1}}
+      />
     </View>
   );
 };
