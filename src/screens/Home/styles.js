608251b18e6ee481799908756eb3e5178e2d6d6f
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-17 19:41:10 -0300

    tornando o mapa sobrescrevivel

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import {Colors} from '../../common/colors/Colors';
+
 import {default as MapViewTemplate} from 'react-native-maps';
 
 export const Container = styled.View`
@@ -6,6 +8,14 @@ export const Container = styled.View`
   height: 100%;
 `;
 
-export const MapView = styled(MapViewTemplate)`
+export const MapView = styled(MapViewTemplate).attrs({
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+})`
   flex: 1;
 `;
