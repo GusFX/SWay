015c8dd86bc0917edd70c793cde22f5b1f300fa3
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-16 21:41:19 -0300

    estilizando componentes

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import {default as MapViewTemplate} from 'react-native-maps';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+export const MapView = styled(MapViewTemplate)`
+  flex: 1;
+`;
