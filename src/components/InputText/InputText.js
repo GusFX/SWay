fc428252a252d2b5f3ad002e0d262303e046822d
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-23 20:50:27 -0300

    adicionando estrutura do input text

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Container, TextInput} from './styles';
 
-const InputText = () => {
-  return <></>;
+const InputText = ({label}) => {
+  return (
+    <Container>
+      <TextInput mode="flat" label={label} onChangeText={() => {}} />
+    </Container>
+  );
 };
 
 export default InputText;
