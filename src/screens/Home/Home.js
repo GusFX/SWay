a52143e1b6dbe8b9769f5ef515b8ea5234b54771
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-23 20:52:04 -0300

    posicionndo corretamente componetes em cima do mapa

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import {InputText, InputView} from './styles';
 import {Container, MapView} from './styles';
 
 const Home = () => {
   return (
-    <Container>
-      <MapView
-        initialRegion={{
-          latitude: -29.6914,
-          longitude: -53.8008,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </Container>
+    <>
+      <Container>
+        <MapView
+          initialRegion={{
+            latitude: -29.6914,
+            longitude: -53.8008,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </Container>
+      <InputView>
+        <InputText label="Teste" />
+      </InputView>
+    </>
   );
 };
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,21 +1,36 @@
 import styled from 'styled-components/native';
-import {Colors} from '../../common/colors/Colors';
+import {StyleSheet} from 'react-native';
+// import {Colors} from '../../common/colors/Colors';
+import {Dimensions} from 'react-native';
 
+import {InputText as InputTextTemplate} from '../../components';
 import {default as MapViewTemplate} from 'react-native-maps';
+
+// const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export const Container = styled.View`
   width: 100%;
   height: 100%;
+  flex: 1;
+`;
+
+export const InputText = styled(InputTextTemplate)``;
+
+export const InputView = styled.View`
+  position: absolute;
+  top: ${windowHeight * 0.2}px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 0 15px 0 15px;
 `;
 
 export const MapView = styled(MapViewTemplate).attrs({
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 })`
   flex: 1;
+  position: relative;
 `;
