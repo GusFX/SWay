5d4eaeb41cafd6f9c34a1e7ab98a65c38038f4ed
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-11-23 21:01:23 -0300

    definindo um tema inicial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './navigation';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import {Colors} from './common/colors/Colors';
 
 export default function Index() {
+  const theme = {
+    ...DefaultTheme,
+    dark: false,
+    roundness: 30,
+    colors: {
+      ...DefaultTheme.Colors,
+      primary: Colors.DEEP_BLUE,
+      background: Colors.WHITE,
+      surface: Colors.WHITE,
+    },
+  };
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <NavigationContainer>
         <Routes />
       </NavigationContainer>
