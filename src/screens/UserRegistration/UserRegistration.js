5412e24f0d930676bd25bc537eb34dfa3b6a5219
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-01 19:54:58 -0300

    populando tela com inputs

--- src/screens/UserRegistration/UserRegistration.js
+++ src/screens/UserRegistration/UserRegistration.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {Form} from '@unform/mobile';
+import {InputText} from '../../components';
 
 const UserRegistration = () => {
-  return <></>;
+  return (
+    <>
+      <Form>
+        <InputText label="Nome" name="nome" />
+        <InputText label="CPF" name="cpf" />
+        <InputText label="E-Mail" name="email" />
+        <InputText label="Senha" name="senha" />
+        <InputText label="Confirme a senha" name="confirmacao" />
+      </Form>
+    </>
+  );
 };
 
 export default UserRegistration;
