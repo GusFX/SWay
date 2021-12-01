f5dcb329ca585203233c2be62470e15439938be7
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-01 20:54:47 -0300

    validando campos

--- src/screens/UserRegistration/UserRegistration.js
+++ src/screens/UserRegistration/UserRegistration.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Form} from '@unform/mobile';
-import {FormView, InputText} from './styles';
-import {Button} from '../../components';
+import {Button, FormView, InputText} from './styles';
+import * as Yup from 'yup';
 
 const UserRegistration = () => {
+  const formRef = useRef();
+
+  const handleSubmit = async data => {
+    try {
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        nome: Yup.string().required(),
+        cpf: Yup.string().length(11).required(),
+        email: Yup.string().email().required(),
+        senha: Yup.string().required(),
+        confirmacao: Yup.string().required(),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      // submitLogin(data);
+      console.log('Dados Validados!!!!');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const validationErrors = {};
+        error.inner.forEach(e => {
+          if (e.path) {
+            validationErrors[e.path] = e.message;
+          }
+        });
+        formRef.current.setErrors(validationErrors);
+      }
+      console.error('erro ao registrar novo usuario: ', error);
+    }
+  };
+
   return (
     <>
       <FormView>
-        <Form>
+        <Form onSubmit={data => handleSubmit(data)} ref={formRef}>
           <InputText label="Nome" name="nome" />
           <InputText label="CPF" name="cpf" />
           <InputText label="E-Mail" name="email" />
@@ -18,7 +49,7 @@ const UserRegistration = () => {
           text="Registrar"
           backgroundColor="DEEP_BLUE"
           type="filled"
-          onPressCallback={() => {}}
+          onPressCallback={() => formRef.current?.submitForm()}
         />
       </FormView>
     </>
--- src/screens/UserRegistration/styles.js
+++ src/screens/UserRegistration/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components/native';
-import {InputText as InputTemplate} from '../../components';
+import {
+  Button as ButtonTemplate,
+  InputText as InputTemplate,
+} from '../../components';
 
+export const Button = styled(ButtonTemplate)``;
 export const InputText = styled(InputTemplate)``;
 
 export const FormView = styled.View`
