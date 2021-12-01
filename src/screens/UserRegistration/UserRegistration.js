bd1394b12f36a88ddf1367d30379acc67ec52826
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-01 20:03:57 -0300

    estilizando formulário

--- src/screens/UserRegistration/UserRegistration.js
+++ src/screens/UserRegistration/UserRegistration.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Form} from '@unform/mobile';
-import {InputText} from '../../components';
+import {FormView, InputText} from './styles';
 
 const UserRegistration = () => {
   return (
     <>
-      <Form>
-        <InputText label="Nome" name="nome" />
-        <InputText label="CPF" name="cpf" />
-        <InputText label="E-Mail" name="email" />
-        <InputText label="Senha" name="senha" />
-        <InputText label="Confirme a senha" name="confirmacao" />
-      </Form>
+      <FormView>
+        <Form>
+          <InputText label="Nome" name="nome" />
+          <InputText label="CPF" name="cpf" />
+          <InputText label="E-Mail" name="email" />
+          <InputText label="Senha" name="senha" />
+          <InputText label="Confirme a senha" name="confirmacao" />
+        </Form>
+      </FormView>
     </>
   );
 };
