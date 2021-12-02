1e3ccae1d5beb9bc2ad9a095859bee1d31d7f566
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-02 13:58:57 -0300

    populando tela com inputs

---
+++ src/screens/OccurrenceRegistration/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+import {
+  InputText as InputTemplate,
+  Button as ButtonTemplate,
+} from '../../components';
+
+export const Button = styled(ButtonTemplate)``;
+export const InputText = styled(InputTemplate)``;
+
+export const FormView = styled.View`
+  padding: 20px;
+`;
