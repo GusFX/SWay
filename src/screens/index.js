4b8bf9c63825de56f3546910c44a3d7c59450f58
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-02 13:47:00 -0300

    criando base para nova tela

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from './Home';
+export {default as OccurrenceRegistration} from './OccurrenceRegistration';
 export {default as TravelHistory} from './TravelHistory';
 export {default as UserRegistration} from './UserRegistration';
