8bbc1bfd3591d52bf761dcc911776f75825ad550
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-03 22:56:02 -0300

    validando campos de registro de ocorrencia

--- src/screens/OccurrenceRegistration/OccurrenceRegistration.js
+++ src/screens/OccurrenceRegistration/OccurrenceRegistration.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import * as Yup from 'yup';
 import {Form} from '@unform/mobile';
 import {Button, InputText, FormView} from './styles';
+import {useNavigation} from '@react-navigation/core';
 
 const OccurrenceRegistration = () => {
+  const formRef = useRef();
+  const navigation = useNavigation();
+
+  const handleSubmit = async data => {
+    try {
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        gravidade: Yup.string().required(),
+        acontecimento: Yup.string().length(11).required(),
+        data: Yup.string().required(),
+        hora: Yup.string().required(),
+        local: Yup.string().required(),
+        detalhes: Yup.string(),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      // submitLogin(data);
+      console.log('Dados Validados!!!!');
+      navigation.navigate('Home');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const validationErrors = {};
+        error.inner.forEach(e => {
+          if (e.path) {
+            validationErrors[e.path] = e.message;
+          }
+        });
+        formRef.current.setErrors(validationErrors);
+      }
+      console.error('erro ao registrar ocorrencia: ', error);
+    }
+  };
+
   return (
     <FormView>
-      <Form>
+      <Form onSubmit={data => handleSubmit(data)} ref={formRef}>
         <InputText label="Gravidade da Ocorrência" name="gravidade" />
         <InputText label="Qual foi o acontecimento" name="acontecimento" />
         <InputText label="Data" name="data" />
@@ -17,7 +53,7 @@ const OccurrenceRegistration = () => {
         text="Registrar"
         backgroundColor="DEEP_BLUE"
         type="filled"
-        onPressCallback={() => {}}
+        onPressCallback={() => formRef.current?.submitForm()}
       />
     </FormView>
   );
