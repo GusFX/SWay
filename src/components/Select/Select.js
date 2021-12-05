873c8240e7cb56b12f8ca629f75c6b18df149c39
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-05 20:50:39 -0300

    Criando componente Select

---
+++ src/components/Select/Select.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {DropDownView} from './styles';
+import {TSelect} from './@types';
+import {useField} from '@unform/core';
+import {default as DropDown} from 'react-native-paper-dropdown';
+
+const Select = props => {
+  const {name} = props;
+  const selectRef = useRef(null);
+  const {error, fieldName, registerField, defaultValue = ''} = useField(name);
+
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(defaultValue);
+
+  const _handleChangeValue = React.useCallback(
+    val => {
+      setSelectedValue(val);
+      props.onSelectionChange(val);
+    },
+    [props],
+  );
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: selectRef.current,
+      getValue: () => {
+        return selectedValue || '';
+      },
+      setValue: (ref, value) => {
+        _handleChangeValue(value);
+      },
+      clearValue: () => {
+        _handleChangeValue('');
+      },
+    });
+  }, [fieldName, registerField, selectedValue, _handleChangeValue]);
+
+  return (
+    <DropDownView>
+      <DropDown
+        label={props.label}
+        mode={props.mode}
+        visible={showDropDown}
+        showDropDown={() =>
+          props.disabled ? setShowDropDown(false) : setShowDropDown(true)
+        }
+        onDismiss={() => setShowDropDown(false)}
+        value={selectedValue}
+        setValue={val => {
+          _handleChangeValue(val);
+        }}
+        list={props.options}
+        inputProps={{
+          disabled: props.disabled,
+          error: Boolean(error),
+        }}
+      />
+    </DropDownView>
+  );
+};
+
+export default Select;
