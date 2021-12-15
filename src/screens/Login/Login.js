c388541ebef6ede86c79a8744edcbe9a22261e51
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2021-12-15 20:59:17 -0300

    validando campos de login

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -9,6 +9,7 @@ import {
 } from './styles';
 import {Form} from '@unform/mobile';
 import {useNavigation} from '@react-navigation/core';
+import * as Yup from 'yup';
 
 const Login = () => {
   const formRef = useRef();
@@ -18,6 +19,30 @@ const Login = () => {
     navigation.navigate(route);
   };
 
+  const handleSubmit = async data => {
+    try {
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        senha: Yup.string().min(8).required(),
+      });
+
+      await schema.validate(data, {abortEarly: false});
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const validationErrors = {};
+        error.inner.forEach(e => {
+          if (e.path) {
+            validationErrors[e.path] = e.message;
+          }
+        });
+        formRef.current.setErrors(validationErrors);
+      } else {
+        console.error('erro ao fazer login: ', error);
+      }
+    }
+  };
+
   return (
     <>
       <TitleView>
@@ -25,14 +50,14 @@ const Login = () => {
       </TitleView>
 
       <FormView>
-        <Form onSubmit={() => {}} ref={formRef}>
+        <Form onSubmit={data => handleSubmit(data)} ref={formRef}>
           <InputText label="E-Mail" name="email" />
           <InputText label="Senha" name="senha" />
           <Button
             text="Entrar"
             backgroundColor="DEEP_BLUE"
             type="filled"
-            onPressCallback={() => {}}
+            onPressCallback={() => formRef.current.submitForm()}
           />
         </Form>
         <TouchableText onPress={() => handleNavigate('UserRegistration')}>
