84e6d9329691a4959ff85c6efa20cba4553a0a7f
Gustavo Felix do Nascimento <gfnascimento@inf.ufsm.br>
2022-01-12 19:23:42 -0300

    renomeando variaveis

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,8 +3,6 @@ import {useNavigation} from '@react-navigation/core';
 import {Button, InputView, ListView, OcurrenceView, MarkerText} from './styles';
 import {Container, MapView} from './styles';
 
-// import MapViewDirections from 'react-native-maps-directions';
-// import getDirections from 'react-native-google-maps-directions';
 // import {PermissionsAndroid} from 'react-native';
 
 // import Geocoder from 'react-native-geocoding';
@@ -12,12 +10,8 @@ import {Container, MapView} from './styles';
 const Home = () => {
   const navigation = useNavigation();
 
-  const [origin] = useState({
-    latitude: -29.6914,
-    longitude: -53.8008,
-  });
-
-  const [destiny] = useState({latitude: -29.689294, longitude: -53.798107});
+  const [origin] = useState({latitude: -29.6914, longitude: -53.8008});
+  const [destination] = useState({latitude: -29.689294, longitude: -53.798107});
 
   return (
     <>
@@ -34,7 +28,7 @@ const Home = () => {
               <MarkerText>Origem</MarkerText>
             </MapView.Callout>
           </MapView.Marker>
-          <MapView.Marker coordinate={destiny}>
+          <MapView.Marker coordinate={destination}>
             <MapView.Callout>
               <MarkerText>Destino</MarkerText>
             </MapView.Callout>
